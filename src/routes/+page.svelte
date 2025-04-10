f9378cf417dc7e70a86dd8c91d57dbfb4d4b625d
Wilbert <wilbcorn@gmail.com>
2025-04-11 02:44:19 +0800

    designed nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Navbar from '$lib/components/navbar.svelte';
+</script>
+
+<div class="min-h-screen">
+    <Navbar/>
+</div>
